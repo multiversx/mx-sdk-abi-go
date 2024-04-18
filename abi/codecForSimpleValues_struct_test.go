f5966877e919577e4456e4af21f13622cb4b7009
Andrei Băncioiu <andrei.bancioiu@gmail.com>
2024-04-18 15:43:18 +0300

    Move tests for enum and struct.

---
+++ abi/codecForSimpleValues_struct_test.go
@@ -0,0 +1,95 @@
+package abi
+
+import (
+	"testing"
+)
+
+func TestCodec_Struct(t *testing.T) {
+	codec, _ := newCodec(argsNewCodec{
+		pubKeyLength: 32,
+	})
+
+	t.Run("should encode nested", func(t *testing.T) {
+		testEncodeNested(t, codec,
+			StructValue{
+				Fields: []Field{
+					{
+						Value: U8Value{Value: 0x01},
+					},
+					{
+						Value: U16Value{Value: 0x4142},
+					},
+				},
+			},
+			"014142",
+		)
+	})
+
+	t.Run("should encode top-level", func(t *testing.T) {
+		testEncodeTopLevel(t, codec,
+			StructValue{
+				Fields: []Field{
+					{
+						Value: U8Value{Value: 0x01},
+					},
+					{
+						Value: U16Value{Value: 0x4142},
+					},
+				},
+			},
+			"014142",
+		)
+	})
+
+	t.Run("should decode nested", func(t *testing.T) {
+		testDecodeNested(t, codec,
+			"014142",
+			&StructValue{
+				Fields: []Field{
+					{
+						Value: &U8Value{},
+					},
+					{
+						Value: &U16Value{},
+					},
+				},
+			},
+			&StructValue{
+				Fields: []Field{
+					{
+						Value: &U8Value{Value: 0x01},
+					},
+					{
+						Value: &U16Value{Value: 0x4142},
+					},
+				},
+			},
+		)
+	})
+
+	t.Run("should decode top-level", func(t *testing.T) {
+		testDecodeTopLevel(t, codec,
+			"014142",
+			&StructValue{
+				Fields: []Field{
+					{
+						Value: &U8Value{},
+					},
+					{
+						Value: &U16Value{},
+					},
+				},
+			},
+			&StructValue{
+				Fields: []Field{
+					{
+						Value: &U8Value{Value: 0x01},
+					},
+					{
+						Value: &U16Value{Value: 0x4142},
+					},
+				},
+			},
+		)
+	})
+}
